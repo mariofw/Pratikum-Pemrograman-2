414041f77eb307a8f003ae884bba5b6b6dea03b0
mariofw <mariofranca1968@gmail.com>
2023-09-15 21:46:29 +0800

    Delete src directory

--- src/PRAK104.java
+++
@@ -1,8 +0,0 @@
-public class PRAK104 {
-    public static void main(String[] args) {
-        String nama = "Mario Franca Wijaya";
-        int umur = 19;
-
-        System.out.println("Perkenalkan, Nama Saya "+ nama +", Umur Saya "+ umur);
-    }
-}
