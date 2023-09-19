03ed10e021a0a9c857a553be4312a1e5fe323d30
mariofw <mariofranca1968@gmail.com>
2023-09-19 18:59:45 +0800

    Delete Pratikum1 directory

--- Pratikum1/src/PRAK101.java
+++
@@ -1,7 +0,0 @@
-public class PRAK101 {
-    public static void main(String[] args) {
-        System.out.printf("Selamat pagi, Mario Franca Wijaya\n");
-        System.out.printf("Selamat siang, Mario Franca Wijaya\n");
-        System.out.printf("Selamat malam, Mario Franca Wijaya");
-    }
-}
