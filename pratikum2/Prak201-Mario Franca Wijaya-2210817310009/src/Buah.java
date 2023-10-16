5838e7d07e92f4b4df4403af58f3e82297f6d0d1
Mario Franca Wijaya <mariofranca1968@gmail.com>
2023-10-16 21:50:51 +0800

    Pratikum2

--- pratikum2/Prak201-Mario Franca Wijaya-2210817310009/src/Buah.java
+++ pratikum2/Prak201-Mario Franca Wijaya-2210817310009/src/Buah.java
@@ -17,7 +17,7 @@ public class Buah {
         public void display(){
             System.out.println("Nama Buah: "+nama);
             System.out.println("Berat: "+berat+"KG");
-            System.out.println("Harga: Rp"+harga);
+            System.out.println("Harga: "+harga);
             System.out.println("Harga Sebelum Diskon: Rp"+hargaSebelumDiskon);
             System.out.println("Total Diskon: Rp"+totalDiskon);
             System.out.println("Harga Setelah Diskon: Rp"+hargaSesudahDiskon+"\n");
