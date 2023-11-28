6598d6b8c66dcdb27a061314df5b22e99c4f0f4e
Mario Franca Wijaya <mariofranca1968@gmail.com>
2023-11-28 13:47:12 +0800

    pratikum

---
+++ pratikum5/src/soal1/Cylinder.java
@@ -0,0 +1,23 @@
+package soal1;
+
+public class Cylinder extends Shape{
+    private double radius;
+    private double height;
+
+
+    public Cylinder(double r, double h) {
+        super("Cylinder");
+        this.radius = r;
+        this.height = h;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI*radius*radius*height;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " of radius " + radius + " and height " + height;
+    }
+}
---
+++ pratikum5/src/soal1/Rectangle.java
@@ -0,0 +1,22 @@
+package soal1;
+
+public class Rectangle extends Shape{
+    private double length;
+    private double width;
+
+    public Rectangle(double l, double w) {
+        super("Rectangle");
+        this.length = l;
+        this.width = w;
+    }
+
+    @Override
+    public double area() {
+        return length * width;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " of length " + length + " and width " + width;
+    }
+}
